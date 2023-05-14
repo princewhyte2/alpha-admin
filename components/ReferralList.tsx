882416c44c001ef7d710bdb2a4beaf9f0e3efb5d
princewhyte dabotubo <princewhyte02@gmail.com>
2023-05-14 17:47:34 +0100

    referral email

--- components/ReferralList.tsx
+++ components/ReferralList.tsx
@@ -8,9 +8,11 @@ export const ReferralList = () => (
       <DateField source="created_at" />
       <NumberField source="user.id" />
       <TextField source="user.first_name" />
+      <TextField source="user.email" />
       {/* <ReferenceField source="user.id" reference="users" /> */}
       <NumberField source="referred_by.id" />
       {/* <ReferenceField source="referred_by.id" reference="users" /> */}
+      <TextField source="referred_by.email" />
       <TextField source="referred_by.first_name" />
       <TextField source="referred_by.referral_point" />
     </Datagrid>
