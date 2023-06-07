c65abdea8413c32afcb066be14d5a80fe77909b8
princewhyte dabotubo <princewhyte02@gmail.com>
2023-06-07 18:04:53 +0100

    fix:success notification

--- components/UsersList.tsx
+++ components/UsersList.tsx
@@ -14,12 +14,19 @@ import {
   downloadCSV,
   ReferenceInput,
   DeleteWithConfirmButton,
+  useNotify,
+  Button,
+  useRecordContext,
+  useDelete,
+  Confirm,
+  DeleteButton,
 } from "react-admin"
 import jsonExport from "jsonexport/dist"
 import { MyList } from "./ArtisanLists"
 import PaymentIcon from "@mui/icons-material/Payment"
 import { Stack } from "@mui/material"
 import { PostPagination } from "./OccupationsList"
+import { useState } from "react"
 const postFilters = [
   // <TextInput key="q" label="Search" source="q" alwaysOn />,
   <ReferenceInput key="role" label="Role" source="role" reference="roles" />,
@@ -56,52 +63,88 @@ const exporter = (users: any) => {
   )
 }
 
-export const UserList = () => (
-  <MyList sortable={false} title="Users" exporter={exporter} perPage={15} filters={postFilters}>
-    <Datagrid
-      bulkActionButtons={false}
-      sx={{
-        // backgroundColor: "Lavender",
-        "& .RaDatagrid-headerCell": {
-          backgroundColor: "#3E4095",
-          color: "white",
-        },
-      }}
-      // rowClick="edit"
-    >
-      <TextField sortable={false} source="id" />
-      <TextField sortable={false} source="title" />
-      <TextField sortable={false} source="first_name" />
-      <TextField sortable={false} source="middle_name" />
-      <TextField sortable={false} source="last_name" />
-      <TextField sortable={false} label="Role" source="user_type" />
-      <EmailField sortable={false} source="email" />
-      <TextField sortable={false} source="gender" />
-      <TextField sortable={false} source="referrer_point" />
-      <DeleteWithConfirmButton
-        icon={<PaymentIcon />}
-        sx={{ color: "green" }}
-        label="Pay"
-        confirmTitle="Make payment"
-        title="Make payment"
-        confirmContent="Are you sure you have made payment to this user?"
+const PayButton = () => {
+  const record = useRecordContext()
+  const [open, setOpen] = useState(false)
+  const notify = useNotify()
 
-        // translateOptions={{ name: record?.referred_by?.first_name }}
+  const [remove, { isLoading }] = useDelete("users", { id: record && record.id })
+
+  const handleClick = () => setOpen(true)
+  const handleDialogClose = () => setOpen(false)
+  const handleConfirm = async () => {
+    try {
+      await remove()
+      notify("users.deleted", { messageArgs: { _: "Payment Successfull" } })
+      setOpen(false)
+    } catch (error: any) {
+      console.log("error")
+    }
+  }
+
+  return (
+    <>
+      <EditButton icon={<PaymentIcon />} sx={{ color: "green" }} label="Pay" onClick={handleClick} />
+      <Confirm
+        isOpen={open}
+        loading={isLoading}
+        title={`Make payment to #${record && record.id}`}
+        content="Are you sure you have made payment to this user?"
+        onConfirm={handleConfirm}
+        onClose={handleDialogClose}
       />
-      <ShowButton />
-      {/* <DateField source="date_of_birth" />
-      <TextField source="referrer_code" />
-      <TextField source="hobbies" />
-      <BooleanField source="has_set_security_question" />
-      <TextField source="phone_number" />
-      <TextField source="other_phone_number" />
-      <TextField source="user_type" />
-      <DateField source="user_type_set_at" />
-      <BooleanField source="has_verified_email" />
-      <BooleanField source="has_verified_phone_number" />
-      <BooleanField source="has_verified_other_phone_number" />
-      <BooleanField source="has_created_company" />
-      <NumberField source="relationships.total_connections" /> */}
-    </Datagrid>
-  </MyList>
-)
+    </>
+  )
+}
+export const UserList = () => {
+  return (
+    <MyList sortable={false} title="Users" exporter={exporter} perPage={15} filters={postFilters}>
+      <Datagrid
+        bulkActionButtons={false}
+        sx={{
+          // backgroundColor: "Lavender",
+          "& .RaDatagrid-headerCell": {
+            backgroundColor: "#3E4095",
+            color: "white",
+          },
+        }}
+        // rowClick="edit"
+      >
+        <TextField sortable={false} source="id" />
+        <TextField sortable={false} source="title" />
+        <TextField sortable={false} source="first_name" />
+        <TextField sortable={false} source="middle_name" />
+        <TextField sortable={false} source="last_name" />
+        <TextField sortable={false} label="Role" source="user_type" />
+        <EmailField sortable={false} source="email" />
+        <TextField sortable={false} source="gender" />
+        <TextField sortable={false} source="referrer_point" />
+        <PayButton />
+        {/* <DeleteWithConfirmButton
+          icon={<PaymentIcon />}
+          sx={{ color: "green" }}
+          label="Pay"
+          confirmTitle="Make payment"
+          title="Make payment"
+          confirmContent="Are you sure you have made payment to this user?"
+
+         
+        /> */}
+        <ShowButton />
+        {/* <DateField source="date_of_birth" />
+        <TextField source="referrer_code" />
+        <TextField source="hobbies" />
+        <BooleanField source="has_set_security_question" />
+        <TextField source="phone_number" />
+        <TextField source="other_phone_number" />
+        <TextField source="user_type" />
+        <DateField source="user_type_set_at" />
+        <BooleanField source="has_verified_email" />
+        <BooleanField source="has_verified_phone_number" />
+        <BooleanField source="has_verified_other_phone_number" />
+        <BooleanField source="has_created_company" />
+        <NumberField source="relationships.total_connections" /> */}
+      </Datagrid>
+    </MyList>
+  )
+}
